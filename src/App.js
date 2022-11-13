162ab96d02854eb487e1283a665e1e7fb7ed4a1d
Alberto Orozco <kirarap@hotmail.com>
2022-11-12 21:34:49 -0800

    Subiendo entrega final react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {useThemeHook} from './GlobalComponents/ThemeProvider'
+import Header from './Components/Header'
+import { Router } from '@reach/router';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import ProductDetails from './Pages/ProductDetails';
+
+
+function App() {
+  const [theme] = useThemeHook()
+  return (
+    <main className={theme? 'bg-black':'bg-light-2'} style={{height: '100vh', overflowY: 'auto'}}>
+      <Header />
+      <Router>
+        <Home path="/" />
+        <ProductDetails path="product/:id" />
+        <Cart path="/cart" />
+      </Router>
+    </main>
+  );
+}
+
+export default App;
